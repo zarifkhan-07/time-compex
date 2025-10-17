8ed1081df5e422cda7a177ab372835016698c0ad
zarifkhan-07 <zarifsuhana1@gmail.com>
2025-10-17 18:38:00 +0600

    Add files via upload

---
+++ O(n)Time.py
@@ -0,0 +1,10 @@
+def Ontime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iteration+=1
+    print("\n When n is",n,"Iteration=",iteration)
+
+Ontime(222222222222)
+Ontime(90)
+
+print("\n N is same as iteration.")
